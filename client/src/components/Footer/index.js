ef8dcbe430bdd2efec8dc26e239e9d97f1f9edde
CarolY <mon_alisa75@yahoo.com>
2021-02-11 10:19:54 -0800

    Set up Footer.

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>&copy; 2021 Carolina Yoedhana</span>
+    </footer>
+  );
+}
+
+export default Footer;
