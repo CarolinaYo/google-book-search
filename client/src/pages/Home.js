7423e09d2c57b8c15c4788de30d085c3b319c7a9
CarolY <mon_alisa75@yahoo.com>
2021-02-12 23:19:15 -0800

    Search function produce results.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { Container } from "../components/Grid/Grid";
+import { Container, Row, Col } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
 import Navbar from "../components/Navbar";
-import Jumbotron from "../components/Jumbotron/Jumbotron";
-import { Input, SubmitBtn } from "../components/Search/Search";
+import { Input, SubmitBtn } from "../components/SearchForm";
 import API from "../utils/API";
-import ResultList from "../components/ResultList/ResultList";
+
+// import ResultList from "../components/ResultList/ResultList";
 
 class Home extends Component {
   state = {
@@ -16,60 +17,66 @@ class Home extends Component {
   bookSearch = () => {
     API.getGoogleBooks(this.state.search)
       .then((res) => {
-        console.log("This is res.data", res);
-        // this.setState({
-        //   books: res.data.items,
-        //   search: "",
+        console.log("This is res.data", res.data.items);
+        this.setState({
+          books: res.data.items,
+          search: "",
         });
       })
       .catch((err) => console.log(err));
   };
 
-  // Create function to handle input data
-//   handleInputChange = (event) => {
-//     const { name, value } = event.target;
-//     this.setState({
-//       [name]: value,
-//     });
-//   };
-
-  // Create function to handle form data submission
-//   handleFormSubmit = (event) => {
-//     event.preventDefault();
-//     this.searchBooks();
-//   };
-
-  saveGoogleBook = (currentBook) => {
-    console.log("This is the current book", currentBook);
-    API.saveBook({
-      id: currentBook.id,
-      title: currentBook.title,
-      authors: currentBook.authors,
-      description: currentBook.description,
-      image: currentBook.image,
-      link: currentBook.link,
-    })
-      .then((res) => console.log("Successful POST to DB!", res))
-      .catch((err) => console.log("this is the error", err));
+  //   Create function to handle input data
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
   };
+
+  //   Create function to handle form data submission
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.bookSearch();
+  };
+
+  //   saveGoogleBook = (currentBook) => {
+  //     console.log("This is the current book", currentBook);
+  //     API.saveBook({
+  //       id: currentBook.id,
+  //       title: currentBook.title,
+  //       authors: currentBook.authors,
+  //       description: currentBook.description,
+  //       image: currentBook.image,
+  //       link: currentBook.link,
+  //     })
+  //       .then((res) => console.log("Successful POST to DB!", res))
+  //       .catch((err) => console.log("this is the error", err));
+  //   };
 
   render() {
     return (
       <div>
-        <Navbar />
         <Container fluid>
+          <Navbar />
           <Jumbotron />
-          <form>
-            <h5>Search for books</h5>
-            <Input
-              value={this.state.search}
-              onChange={this.handleInputChange}
-              name="search"
-              placeholder="Cookbook for Nerds"
-            />
-            <SubmitBtn onClick={this.handleFormSubmit} />
-          </form>
-          <ResultList />
+          <Row>
+            <Col size="md-8">
+              <label>Search books by title</label>
+              <form className="row g-3">
+                <Input
+                  value={this.state.search}
+                  onChange={this.handleInputChange}
+                  name="search"
+                  placeholder="e.g Cookbook for Nerds"
+                />
+                <div>
+                  <SubmitBtn onClick={this.handleFormSubmit} />
+                </div>
+              </form>
+            </Col>
+          </Row>
+          {/* <ResultList /> */}
         </Container>
       </div>
     );
