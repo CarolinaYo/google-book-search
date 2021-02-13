5767fdcb433707824eed96e52ffd9b4b21c9d163
CarolY <mon_alisa75@yahoo.com>
2021-02-12 20:43:28 -0800

    Set jumbotron as title.

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="text-center jumboText">Google Books Search</h1>
+        <br></br>
+        <h3 className="text-center jumboText">
+          Search and Save Books if Interest
+        </h3>
+      </div>
+    </div>
+  );
+}
+export default Jumbotron;
