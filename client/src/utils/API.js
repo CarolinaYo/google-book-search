4d0e0241eec1c267fe9a76a876f12b85085359ea
CarolY <mon_alisa75@yahoo.com>
2021-02-12 23:22:53 -0800

    Draft API calls to search, save and delete.

---
+++ client/src/utils/API.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default {
+  // Gets all books
+  getGoogleBooks: function (query) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  },
+  // Get saved books
+  getSavedBooks: function () {
+    return axios.get("/api/books/");
+  },
+
+  //Get book by id
+  getBook: function (id) {
+    return axios.get("/api/books/" + id);
+  },
+
+  // Saves a book to the database
+  saveBook: function (bookData) {
+    return axios.post("/api/books", bookData);
+  },
+  // Deletes the book with the given id
+  deleteBook: function (id) {
+    return axios.delete("/api/books/" + id);
+  },
+};
